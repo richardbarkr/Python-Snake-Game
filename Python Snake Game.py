ff4e85d36e5e6468f5b43e64d0c4257898871759
Richard Barker <74937909+richardbarkr@users.noreply.github.com>
2021-07-26 16:30:02 +0100

    Add files via upload

---
+++ Python Snake Game.py
@@ -0,0 +1,143 @@
+"""
+Created on Mon Jul 12 19:50:22 2021
+
+@author: RichardBarker
+Tutorial Credit: Python Engineers & CSDojo
+"""
+
+#======================#
+#= Python Snake Game ==#
+#======================#
+
+#--Note: Consider using Replit to view game in terminal
+#        Expanding the terminal window also improves visability
+
+from blessed import Terminal
+import random
+import copy
+from collections import deque
+
+term = Terminal()
+UP = term.KEY_UP
+RIGHT = term.KEY_RIGHT
+LEFT = term.KEY_LEFT
+DOWN = term.KEY_DOWN
+direction = RIGHT
+
+#--CONFIGURE SNAKE--#
+BODY = '⬜️'
+HEAD = '⬜️'
+
+#--CONFIGURE BORDER--#
+BORDER = '🟦'
+SPACE = '  '
+
+#--CONFIGURE FOOD--#
+APPLE = '🍎'
+
+#--INITIAL SNAKE POSITION--#
+snake = deque([[6, 5], [6, 4], [6, 3]])
+HEAD = deque([6,2])
+
+#--INITIAL FOOD POSITION--#
+food = [5, 10]
+h, w = 12, 12
+score = 0
+
+#--INITIAL SPEED--#
+speed = 3
+
+#--MAX SPEED--#
+MAX_SPEED = 6
+
+#--CONFIGURATION END--#
+
+def list_empty_spaces(world, space):
+  result = []
+  for i in range(len(world)):
+    for j in range(len(world[i])):
+      if world[i][j] == space:
+        result.append([i, j])
+  return result
+
+with term.cbreak(), term.hidden_cursor():
+
+#--SCREEN TO RESTART THE GAME--#
+  print(term.home + term.clear)
+  
+#--CONFIGURE THE WORLD--#
+  world = [[SPACE] * w for _ in range(h)]
+  for i in range(h):
+    world[i][0] = BORDER
+    world[i][0] = BORDER
+    world[i][-1] = BORDER
+  for j in range(w):
+    world[0][j] = BORDER
+    world[-1][j] = BORDER
+  for s in snake:
+    world[s[0]][s[1]] = BODY
+  world[food[0]][food[1]] = APPLE
+  for row in world:
+    print(' '.join(row))
+  print('Use arrow keys to move ←↕→')
+  print('')
+  print('Please expand the terminal window ')
+  print('so the game has enough space to run')
+
+  val = ''
+  moving = False
+
+  while val.lower() != 'q':
+    val = term.inkey(timeout=1/speed)
+    if val.code in [UP, RIGHT, DOWN]:
+      moving = True
+    if not moving:
+      continue
+
+    if val.code == UP and direction != DOWN:
+      direction = UP
+    elif val.code == RIGHT and direction != LEFT:
+      direction = RIGHT
+    elif val.code == DOWN and direction != UP:
+      direction = DOWN
+    elif val.code == LEFT and direction != RIGHT:
+      direction = LEFT
+    
+    head = copy.copy(snake[0])
+    if direction == UP:
+      head[0] -= 1
+    elif direction == RIGHT:
+      head[1] += 1
+    elif direction == DOWN:
+      head[0] += 1
+    elif direction == LEFT:
+      head[1] -= 1
+    
+    heading = world[head[0]][head[1]]
+    ate_food = False
+    if heading == APPLE:
+      ate_food = True
+      empty_spaces = list_empty_spaces(world, SPACE)
+      food = random.choice(empty_spaces)
+      world[food[0]][food[1]] = APPLE
+      speed = min(MAX_SPEED, speed * 1.07)
+    elif heading == BORDER:
+      break
+    elif heading == BODY and head != snake[-1]:
+      break
+
+    if not ate_food:
+      tail = snake.pop()
+      world[tail[0]][tail[1]] = SPACE
+    world[head[0]][head[1]] = BODY
+    snake.appendleft(head)
+
+    print(term.move_yx(0, 0))
+    for row in world:
+      print(' '.join(row))
+    
+    score = len(snake) - 3
+    print(f'Score: {score} - Speed: {speed:.1f}   ')
+    print(term.clear_eos, end='')
+
+print('Thanks for playing :)')
